a50dc589dee6e05cb060fa7bec7daf39741e9c95
Rene Juuse <rene.juuse@nortal.com>
2024-05-07 17:58:24 +0300

    fix(check-hider): Hider always reported as already existing

--- hide-and-seek-app/routes/games.js
+++ hide-and-seek-app/routes/games.js
@@ -4,26 +4,14 @@ const Game = require('../models/Game');
 const User = require('../models/User');
 const geolib = require('geolib');
 
-router.post('/start', async (req, res) => {
-  try {
-    const { room } = req.body;
-    
-    const game = await Game.findOneAndUpdate({ room }, { status: 'active' }, { new: true });
-    if (!game) {
-      return res.status(404).send({ message: 'Room not found' });
-    }
-    res.status(200).send({ message: 'Game started', game });
-  } catch (error) {
-    res.status(400).send({ message: 'Error starting game', error: error.message });
-  }
-});
 
 // 1.Check whether a room has a hider
 router.get('/check-hider/:roomId', async (req, res) => {
   try {
     const { roomId } = req.params;
     const hider = await User.findOne({ room: roomId, role: 'hider' });
-    res.status(200).send({ hasHider: !!hider });
+    const game = await Game.findOne({ room: roomId });
+    res.status(200).send({ hasHider: Boolean(!!hider || (hider && game?.status === 'ENDED')) });
   } catch (error) {
     res.status(500).send({ message: 'Error checking for hider', error: error.message });
   }
@@ -33,11 +21,11 @@ router.get('/check-hider/:roomId', async (req, res) => {
 router.post('/lock-hider-location', async (req, res) => {
   const { roomId, location } = req.body;
   try {
-    const approximateLocation = geolib.computeDestinationPoint(location, 1500, Math.random() * 360);
+    const approximateLocation = geolib.computeDestinationPoint(location, Math.random() * 1000, Math.random() * 360);
     await Game.findOneAndUpdate(
       { room: roomId },
       {
-        status: 'active',
+        status: 'STARTED',
         'hiderLocation.exact': location,
         'hiderLocation.approximate': {
           latitude: approximateLocation.latitude,
