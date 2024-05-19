fa75eb099672cdc102e6f6cf6a9645308b2e642b
Rene Juuse <rene.juuse@nortal.com>
2024-05-19 23:16:16 +0300

    build: Removed unused websockets

--- hide-and-seek-app/server.js
+++ hide-and-seek-app/server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const WebSocket = require('ws');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -26,37 +25,4 @@ const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-const wss = new WebSocket.Server({ server });
-
-module.exports = { app, wss };
-
-wss.on('connection', ws => {
-    console.log('Client connected');
-    ws.on('message', message => {
-      const data = JSON.parse(message);
-      handleMessages(data, ws);
-    });
-    ws.on('close', () => {
-      console.log('Client disconnected');
-    });
-  });
-  
-  function handleMessages(data, ws) {
-    switch(data.type) {
-      case 'locationUpdate':
-        broadcastLocation(data);
-        break;
-      case 'gameEvent':
-        handleGameEvent(data, ws);
-        break;
-    }
-  }
-  
-  function broadcastLocation(data) {
-    wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(data));
-      }
-    });
-  }
-  
+module.exports = { app };
